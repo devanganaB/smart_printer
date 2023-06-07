57dd83072188f2877526602db0517ae5ce65d5a1
Devangana26 <devanganabarua@gmail.com>
2023-06-07 18:29:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/getStarted.jpg'),
+                  fit: BoxFit.cover)),
+          child: Center(
+              child: Padding(
+            padding: EdgeInsets.only(bottom: 20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                print('Button Pressed');
+              },
+              child: Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 25, 95, 215)),
+            ),
+          ))),
+    );
+  }
+}
