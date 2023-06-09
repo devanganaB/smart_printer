73045a5565bf9b2bf90367b4d62a21eaedb71175
jaitra29 <135872702+jaitra29@users.noreply.github.com>
2023-06-10 00:30:51 +0530

    registration page

--- lib/Screens/RegistrationPage.dart
+++ lib/Screens/RegistrationPage.dart
@@ -1,54 +1,156 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:smart_printer/Widgets/widgets.dart';
-class RegistrationPage extends StatefulWidget {
-  const RegistrationPage({super.key});
 
+class RegisterPage extends StatefulWidget {
   @override
-  State<RegistrationPage> createState() => _RegistrationPageState();
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
-class _RegistrationPageState extends State<RegistrationPage> {
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController contactController = TextEditingController();
+  TextEditingController branchController = TextEditingController();
+  TextEditingController divisionController = TextEditingController();
+  TextEditingController libraryCardController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  bool obscurePassword = true;
+  bool obscureConfirmPassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Container(
-            height: MediaQuery.sizeOf(context).height,
-            width: MediaQuery.sizeOf(context).width,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xFFA0B5EB), Color(0xFFC9F0E4)])
-            ),
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Register Page',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFEBF0F4),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
               children: [
-                const Text(
-                  'Smart Printer',
-                  style: TextStyle(
-                    fontFamily: 'Times New Roman',
-                    fontSize: 38,
-                    fontWeight: FontWeight.w500,
+                Center(
+                  child: Text(
+                    'Smart Printer',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 50,),
-                const Text(
-                  'Create Account',
-                  style: TextStyle(
-                    fontSize: 18,
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Create Account',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      buildTextField('Name', nameController, Icons.person),
+                      buildTextField('Email', emailController, Icons.email),
+                      buildTextField('Contact Number', contactController, Icons.phone),
+                      buildTextField('Branch', branchController, Icons.business),
+                      buildTextField('Division', divisionController, Icons.location_city),
+                      buildTextField('Library Card No', libraryCardController, Icons.credit_card),
+                      buildPasswordField('Password', passwordController, obscurePassword),
+                      buildPasswordField('Confirm Password', confirmPasswordController, obscureConfirmPassword, isConfirm: true),
+                      SizedBox(height: 8),
+                      Text(
+                        'Password must be at least 6 characters',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            String name = nameController.text;
+                            String email = emailController.text;
+                            String contact = contactController.text;
+                            String branch = branchController.text;
+                            String division = divisionController.text;
+                            String libraryCardNo = libraryCardController.text;
+                            String password = passwordController.text;
+
+                            registerUser(
+                              name,
+                              email,
+                              contact,
+                              branch,
+                              division,
+                              libraryCardNo,
+                              password,
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF5200FF),
+                          onPrimary: Colors.white,
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: Text('Create Your Smart Printer Account'),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Already have an account?',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle sign-in button press
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF5200FF),
+                          onPrimary: Colors.white,
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: Text('Sign-In Now'),
+                      ),
+                    ],
                   ),
                 ),
-                UserFormField(icon: Icon(Icons.person_pin_outlined),labelText: 'Name',),
-                UserFormField(icon: Icon(Icons.email),labelText: 'E-mail',),
-                UserFormField(icon: Icon(Icons.contact_phone),labelText: 'Contact Number',),
-                UserFormField(icon: Icon(Icons.abc),labelText: 'Branch',),
-                UserFormField(icon: Icon(Icons.add_circle_outline_outlined),labelText: 'Division',),
-                UserFormField(icon: Icon(Icons.library_books),labelText: 'Library Card No.',),
-                UserFormField(icon: Icon(Icons.lock),labelText: 'Password',),
-                UserFormField(icon: Icon(Icons.lock),labelText: 'Confirm Password',),
-                ElevatedButton(onPressed: (){}, child: const Text('Create Your Smart Printer Account')),
-                ElevatedButton(onPressed: (){}, child: const Text('Sign-In Now')),
               ],
             ),
           ),
@@ -56,4 +158,117 @@ class _RegistrationPageState extends State<RegistrationPage> {
       ),
     );
   }
+
+  Widget buildTextField(String label, TextEditingController controller, IconData icon) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      width: 264,
+      height: 56,
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Please enter $label';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget buildPasswordField(String label, TextEditingController controller, bool obscureText, {bool isConfirm = false}) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      width: 264,
+      height: 56,
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(Icons.lock),
+          suffixIcon: isConfirm
+              ? GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      obscureConfirmPassword = !obscureConfirmPassword;
+                    });
+                  },
+                  child: Icon(obscureConfirmPassword ? Icons.visibility_off : Icons.visibility),
+                )
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      obscurePassword = !obscurePassword;
+                    });
+                  },
+                  child: Icon(obscurePassword ? Icons.visibility_off : Icons.visibility),
+                ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Please enter $label';
+          }
+          if (isConfirm && value != passwordController.text) {
+            return 'Passwords do not match';
+          }
+          if (!isConfirm && value.length < 6) {
+            return 'Password must be at least 6 characters';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  void registerUser(
+    String name,
+    String email,
+    String contact,
+    String branch,
+    String division,
+    String libraryCardNo,
+    String password,
+  ) {
+    // Perform registration logic here
+
+    if (password != confirmPasswordController.text) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Registration Error'),
+          content: Text('Password and Confirm Password do not match.'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    // Perform further validation and registration process
+
+    // Placeholder code
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: RegisterPage(),
+  ));
 }
