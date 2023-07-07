c7777c85122ef846ad3eaa8e7f5499ac5c8d8c3f
Devangana26 <devanganabarua@gmail.com>
2023-07-07 21:42:58 +0530

    overflow checked#2

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Smart Printer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
